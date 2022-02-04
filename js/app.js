f6de1dfa2e023faf4319c1e26f86587ecfbb022a
alemica97 <alessandro.micalizzi08@gmail.com>
2022-02-04 16:13:12 +0100

    ho iniziato l'esercizio dichiarando delle variabili da far inserire all'utente come km ed età

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//chiedo all'utente quanti chilometri vuole percorrere
+const km = prompt('Ciao, quanti chilometri vuoi percorrere?');
+console.log(km);
+
+//chiedo all'utente la sua età
+const userAge = prompt('Quanti anni hai?');
+console.log(userAge);
+
+const priceForKm = (0.21 * km);
+console.log(priceForKm);
