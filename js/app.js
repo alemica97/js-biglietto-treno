77f50ae9a53fc1c9e63d13b67d9d9c5509e6dbf8
alemica97 <alessandro.micalizzi08@gmail.com>
2022-02-04 16:34:18 +0100

    Ho aggiunto una variabile per il prezzo scontato che cambia a seconda dell'età dell'utente

--- js/app.js
+++ js/app.js
@@ -6,5 +6,23 @@ console.log(km);
 const userAge = prompt('Quanti anni hai?');
 console.log(userAge);
 
+//in una variabile salvo il prezzo del biglietto senza sconti
 const priceForKm = (0.21 * km);
 console.log(priceForKm);
+
+//Se l'utente ha meno di 18 anni avrà uno sconto del 20%
+//Se l'utente ha più di 65 anni avrà uno sconto del 40%
+//Altrimenti non avrà nessuno sconto
+
+let discountedTicket;
+
+if (userAge < 18){
+    discountedTicket = priceForKm - ((20 / 100) * priceForKm);
+} else if(userAge > 65){
+    discountedTicket = priceForKm - ((40 / 100) * priceForKm);
+}else{
+    discountedTicket = priceForKm; 
+}
+
+console.log(discountedTicket);
+
