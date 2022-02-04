570ac5a179a7b3a885387ca1b14f3c4411658bd6
alemica97 <alessandro.micalizzi08@gmail.com>
2022-02-04 17:38:20 +0100

    Ho finito l'esercizio

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
+//chiedo il nome all'utente
+const userName = prompt('Ciao, come ti chiami?');
+
 //chiedo all'utente quanti chilometri vuole percorrere
-const km = parseInt(prompt('Ciao, quanti chilometri vuoi percorrere?'));
+const km = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
 console.log(`vuoi percorrere ${km}km`);
 
 //chiedo all'utente la sua età
 const userAge = parseInt(prompt('Quanti anni hai?'));
 console.log(`hai ${userAge} anni`);
 
+document.getElementById('saluto').innerHTML += `Ciao ${userName}, quindi hai ${userAge} anni e vuoi percorrere ${km}km`
 
 //in una variabile salvo il prezzo del biglietto senza sconti
 const priceForKm = (0.21 * km);
@@ -17,18 +21,22 @@ let finalPrice;
 //Se l'utente ha meno di 18 anni avrà uno sconto del 20%
 if (userAge < 18){
     finalPrice = priceForKm - ((20 / 100) * priceForKm);
+    document.writeln('hai avuto uno sconto del 20%');
 //Se l'utente ha più di 65 anni avrà uno sconto del 40%
 } else if(userAge > 65){
     finalPrice = priceForKm - ((40 / 100) * priceForKm);
+    document.writeln('hai avuto uno sconto del 40%');
 //Altrimenti non avrà nessuno sconto
 }else{
     finalPrice = priceForKm; 
+    document.writeln('non hai avuto uno sconto');
 }
+console.log(`il prezzo scontato è ${finalPrice}`)
 
-//faccio in modo che dopo la virgola ci siano solo due cifre
+//faccio in modo che dopo la virgola ci siano solo due cifre, arrotondando per eccesso
 finalPrice = finalPrice.toFixed(2);
 console.log(`il prezzo finale è ${finalPrice}`);
 
 const htmlElement = document.getElementById('price');
 
-htmlElement.innerHTML  += ` ${finalPrice}`;
+htmlElement.innerHTML  += `Vista la tua età, il prezzo finale è ${finalPrice}€`;
